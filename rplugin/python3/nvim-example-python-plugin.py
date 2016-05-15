ff6c55483d58d22e37e0e7640db4b33e8ad12fd1
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-16 01:30:04 +0900

    Slack posting

--- rplugin/python3/nvim-example-python-plugin.py
+++ rplugin/python3/nvim-example-python-plugin.py
@@ -1,11 +1,28 @@
 import neovim
+from slackclient import SlackClient
 
 @neovim.plugin
 class Main(object):
-    def __init__(self, vim):
-        self.vim = vim
+    def __init__(self, nvim):
+        self.nvim = nvim
 
-    @neovim.function('DoItPython')
-    def doItPython(self, args):
-        self.vim.command('echo "hello"')
+    @neovim.command("PostToSlack", range='')
+    def post_to_slack(self, range):
+        b = self.nvim.current.buffer
+        lines = b[(range[0]-1):range[1]]
 
+        text = "\n".join(lines)
+
+        self.nvim.command('echo "Sending it to Slack"')
+
+        token = self.nvim.eval('g:slack_api_token')
+        channel = self.nvim.eval('g:slack_post_channel')
+        self.nvim.command('echo "%s"' % token)
+        sc = SlackClient(token)
+        sc.api_call(
+            'chat.postMessage',
+            channel=channel,
+            text=text,
+            username='vimbot',
+            icon_emoji=':vim:'
+        )
